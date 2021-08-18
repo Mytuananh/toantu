5dd61ad53e29d3998eb9c39352ceba569329383e
Your Name <you@example.com>
2021-08-19 00:29:46 +0700

    toantu

---
+++ toantuth.js
@@ -0,0 +1,10 @@
+function toantu() {
+    let aaa;
+    let bbb;
+    aaa = prompt('nhap so aaa','aaa =');
+    bbb = prompt('nhap so bbb','bbb =');
+    aaa = parseInt(aaa);
+    bbb = parseInt(bbb);
+    let ccc = aaa*bbb;
+    document.write('Diện tích là: ' + ccc)
+}
